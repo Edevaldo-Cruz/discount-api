e3dd4c288990a863f99d1349bb1ea6815e7cb3c9
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2025-05-20 08:17:02 -0300

    feat: atualizar dependências e melhorar configurações do TypeScript

---
+++ custom.d.ts
@@ -0,0 +1,8 @@
+// custom.d.ts
+declare module 'express';
+declare module 'morgan';
+declare module 'cors';
+declare module 'swagger-jsdoc'; 
+declare module 'swagger-ui-express';
+declare module 'jsonwebtoken';
+declare module 'bcryptjs';
